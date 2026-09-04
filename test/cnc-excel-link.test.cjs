d7db0a81973cf3859bc6bf06fd450b71985213a5
matthewlakerdis-dev <matthewlakerdis@gmail.com>
2026-09-04 16:28:01 +1000

    Move CNC export links above table

--- test/cnc-excel-link.test.cjs
+++ test/cnc-excel-link.test.cjs
@@ -6,6 +6,8 @@ const path=require('node:path');
 test('Web CNC tracker links to the live Excel tracker',()=>{
   const html=fs.readFileSync(path.join(__dirname,'../index.html'),'utf8');
   assert.match(html,/const excelLink = `https:\/\/cnc\.panelstockhq\.com\/cnc-tracker\?token=/);
-  assert.match(html,/Excel tracker/);
+  assert.match(html,/" Excel"/);
   assert.match(html,/href: excelLink/);
+  assert.match(html,/linkCopied \? "Link copied!" : "CNC Tracker"/);
+  assert.match(html,/className: "flex flex-wrap items-center gap-2 mb-3"/);
 });
